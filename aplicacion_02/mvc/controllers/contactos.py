93bb4abb5a15ef441fb19c08a2e1540717bcb0cc
JuanCarlos225 <122801328+JuanCarlos225@users.noreply.github.com>
2024-02-12 07:59:59 -0600

    Add files via upload

--- aplicacion_02/mvc/controllers/contactos.py
+++ aplicacion_02/mvc/controllers/contactos.py
@@ -1,11 +1,23 @@
-import web
-render = web.template.render('mvc/views/')
+"""
+    Archivo el cuál se encargará de manejar las operaciones sobre los datos
+"""
+import web  # Importamos el módulo
+from ..models.modelo_contactos import ModeloContactos  # Importamos la clase correspondiente del módelo
+
+CONTACTOS = ModeloContactos()  # Creamos una objeto de la clase ModeloContactos()
+
+render = web.template.render('mvc/views/')  # Obtenemos la ruta donde se renderiza las plantiallas
+
+
+# Creamos una clase llamada Contactos
 class Contactos:
+    # Función que obtendrá todos los contacots
     def GET(self):
         try:
-            return render.contactos()
+            contactos = CONTACTOS.obtenerContactos()
+
+            return render.contactos(contactos)
         except Exception as e:
-            print(f"Error 101- contactos {e.args}")
-            return "Ups algo salio mal con los contactos"
-        
-    
+            print(f"Error 101 - Contactos {e.args}")
+
+            return "Ocurrió algún error"
